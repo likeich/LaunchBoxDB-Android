b688116d42cf6ffb354b777f8f165fd1b21ca86f
Likeich <eichlinkyle@gmail.com>
2023-09-25 18:42:54 -1000

    Update build.gradle.kts

--- LaunchBox DB/build.gradle.kts
+++ LaunchBox DB/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -41,4 +42,20 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                groupId = "com.kyleichlin"
+                artifactId = "launchboxdb"
+                version = ".1"
+
+                afterEvaluate {
+                    from(components["release"])
+                }
+            }
+        }
+    }
 }
